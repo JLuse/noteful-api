7741a438dda0d4a6942a90f389e39eaa7926871d
Jordan Luse <j.luse7@gmail.com>
2021-02-22 17:06:06 -0800

    Added ssl to server file

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const { PORT, DATABASE_URL} = require('./config')
 
 const db = knex({
     client: 'pg',
-    connection: DATABASE_URL
+    connection: DATABASE_URL,
+    ssl: true
   })
+// const db = new pg.Client({
+//   user: "admin",
+//   password: "guest",
+//   database: "Employees",
+//   port: 5432,
+//   host: "localhost",
+//   ssl: true
+// }); 
 
   app.set('db', db)
 
